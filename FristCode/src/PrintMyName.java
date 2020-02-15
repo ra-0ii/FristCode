e561c97275f230238a5f21c2e0a05070ca294875
raa95 <raa95@LAPTOP-FBCRSHPB>
2020-02-15 19:04:21 +0300

    Final commit

--- FristCode/src/PrintMyName.java
+++ FristCode/src/PrintMyName.java
@@ -4,6 +4,7 @@ public class PrintMyName {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("My name is Riyadh");
+		System.out.println("This is my last name: alaamri");
 
 	}
 
